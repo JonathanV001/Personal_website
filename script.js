f1ac5d27260c42dd67795ffd363863bb13b4fbd3
jonathan <jvacaram@uncc.edu>
2024-04-12 21:10:56 -0400

    Layout was adjusted and sidemenu buttons are working

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 
-window.addEventListener("DOMContentLoaded", (event) => {
+document.addEventListener("DOMContentLoaded", function() {
 
 var hamburgerButton = document.getElementById("hamburgerMenu");
 var clicked = false;
 let opacity = 0;
 
 hamburgerButton.addEventListener("click", dropMenu);
+
+function resetDropMenu(){
+    document.getElementById("portraitSideBar").style.display = "none"
+        document.getElementById("portraitSideBar").style.opacity = "0"
+        /*COOL FADE EFFECT!*/
+        opacity = 0;
+        clicked = false;
+}
 
 function dropMenu(){
     if(clicked == false){
@@ -21,11 +29,7 @@ function dropMenu(){
          }, 10);
         console.log(clicked);
     }else{
-        clicked = false;
-        document.getElementById("portraitSideBar").style.display = "none"
-        document.getElementById("portraitSideBar").style.opacity = "0"
-        /*COOL FADE EFFECT!*/
-        opacity = 0;
+        resetDropMenu();
     }
 }
 
@@ -34,16 +38,62 @@ function dropMenu(){
     document.getElementById("portraitSideBar").style.display = "none"
 });*/
 
+/*function for closing sidebar on resize to landscape*/
 window.addEventListener("resize",(event) => {
     var H = parseInt(window.innerHeight);
     var W = parseInt(window.innerWidth);
     if(H > W){
-        document.getElementById("portraitSideBar").style.display = "none"
-        document.getElementById("portraitSideBar").style.opacity = "0"
-        /*COOL FADE EFFECT!*/
-        opacity = 0;
-        clicked = false;
+        resetDropMenu();
     }
 });
+
+/*sidebar button functions*/
+var homeContent = document.getElementById("homeContent");
+var projectsContent = document.getElementById("projectsContent");
+var experienceContent = document.getElementById("experienceContent");
+var resumeContent = document.getElementById("resumeContent");
+
+/*one button for portrait and the other for landscape*/
+document.getElementById("homeIconPor").addEventListener("click", homeButtonClick);
+document.getElementById("homeIconLand").addEventListener("click", homeButtonClick);
+function homeButtonClick(){
+    resetDropMenu();
+    console.log("clciked");
+    homeContent.style.display = "flex";
+    projectsContent.style.display = "none";
+    experienceContent.style.display = "none";
+    resumeContent.style.display = "none";
+}
+
+document.getElementById("projectButtonPor").addEventListener("click", projectButtonClick);
+document.getElementById("projectButtonLand").addEventListener("click", projectButtonClick);
+function projectButtonClick(){
+    resetDropMenu();
+    console.log("clciked");
+    homeContent.style.display = "none";
+    projectsContent.style.display = "flex";
+    experienceContent.style.display = "none";
+    resumeContent.style.display = "none";
+}
+
+document.getElementById("experienceButtonPor").addEventListener("click", experienceButtonClick);
+document.getElementById("experienceButtonLand").addEventListener("click", experienceButtonClick);
+function experienceButtonClick(){
+    resetDropMenu();
+    homeContent.style.display = "none";
+    projectsContent.style.display = "none";
+    experienceContent.style.display = "flex";
+    resumeContent.style.display = "none";
+}
+
+document.getElementById("resumeButtonPor").addEventListener("click", resumeButtonClick);
+document.getElementById("resumeButtonLand").addEventListener("click", resumeButtonClick);
+function resumeButtonClick(){
+    resetDropMenu();
+    homeContent.style.display = "none";
+    projectsContent.style.display = "none";
+    experienceContent.style.display = "none";
+    resumeContent.style.display = "flex";
+}
 
 });
